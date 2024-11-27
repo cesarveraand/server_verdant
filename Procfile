b2ddbb49f416e25b3af8514abff416c1692aeb24
Cesar Vera <soymateovera@gmail.com>
2024-11-27 03:02:29 -0400

    arreglo  server flsk

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python index.py
+web: gunicorn -b 0.0.0.0:$PORT index:app
--- index.py
+++ index.py
@@ -75,5 +75,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(debug=False)  # Cambiar a False o eliminar el parámetro debug
+
